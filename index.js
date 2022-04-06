f98ff98f100831ba7a9ad98c2c0306f8728b5342
oeanb <oeanb12@gmail.com>
2022-04-06 09:58:20 +0900

    graph3

--- index.js
+++ index.js
@@ -93,3 +93,62 @@ var legend = chart.children.push(am5.Legend.new(root, {
 }));
 
 legend.data.setAll(series.dataItems);
+
+/*Graph3*/
+Highcharts.chart('container2', {
+  chart: {
+      type: 'spline'
+  },
+  title: {
+      text: 'Monthly Average Temperature'
+  },
+ 
+  xAxis: {
+      categories: ['1월', '3월', '6월', '9월', '12월', '']
+  },
+  yAxis: {
+      title: {
+          text: 'Temperature'
+      },
+      labels: {
+          formatter: function () {
+              return this.value + '°';
+          }
+      }
+  },
+  tooltip: {
+      crosshairs: true,
+      shared: true
+  },
+  plotOptions: {
+      spline: {
+          marker: {
+              radius: 4,
+              lineColor: '#666666',
+              lineWidth: 1
+          }
+      }
+  },
+  series: [{
+      name: '자연광',
+      marker: {
+          enabled: false
+      },
+      data: [7.0, 25.0, 50.0, {
+          y: 89,
+          marker: { 
+              enabled: false
+          }
+      }, 56.3, 11.0]
+
+  }, {
+      name: '인공지능',
+      color: 'red',
+      marker: {
+          enabled: false
+      },
+      data: [60.0, 61.7, 65.9,{
+          y: 80.5,
+      }, 65.2, 60.5]
+  }]
+});
