fd2c3811baa0074c5b40d4aadabbdd6122112dc7
oeanb <oeanb12@gmail.com>
2022-07-12 13:30:03 +0900

    end

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 /*navBar 날짜 표시*/
-var today = new Date();
-var year = today.getFullYear();
-var month = ('0' + (today.getMonth() + 1)).slice(-2);
-var day = ('0' + today.getDate()).slice(-2);
+let today = new Date();
+let year = today.getFullYear();
+let month = ('0' + (today.getMonth() + 1)).slice(-2);
+let day = ('0' + today.getDate()).slice(-2);
+let json_data;
+GET_value();
 
-var dateString = year + '. ' + month + '. ' + day + '.';
-document.querySelector('.date').innerHTML = dateString;
+update_Graph2();
+update_Graph3();
+update_Graph4();
+update_time();
+
+let le;
+
+function GET_value() {
+    var js_timer1 = 0; 
+    js_timer1 = setTimeout("GET_value()",1000);// 1초 단위로 갱신 처리
+}
+
+function update_time() {
+    let js_timer3 = 0; 
+    // let dateString = 'mac id = ' + json_data + ' 입니다.';
+    // document.querySelector('.date').innerHTML = get_data;
+    let dateString = year + '. ' + month + '. ' + day + '.';
+    document.querySelector('.date').innerHTML = dateString;
+
+    js_timer3 = setTimeout("update_time()",3000);// 3초 단위로 갱신 처리
+}
 
 /*Map API*/
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+let mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
         center: new kakao.maps.LatLng(37.640695, 127.147675), // 지도의 중심좌표
         level: 11 // 지도의 확대 레벨
     };
-
-var OriginalLocation = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+let OriginalLocation = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 // 마커가 표시될 위치입니다 
-var positions = [
+let positions = [
   {
     title: '서울건설기계(주)',
     latLng: new kakao.maps.LatLng(37.673613, 126.865862)
@@ -76,6 +96,7 @@ var positions = [
   }
 ];
 
+
 /*각 기업 클릭 시 위치 마커 생성*/
 for(let i = 0; i < 14; i++) {
     document.querySelector('.li' + i).addEventListener('click', function(e) {
@@ -92,12 +113,16 @@ for(let i = 0; i < 14; i++) {
             position: positions[i].latLng,
             title: positions[i].title
         });
+        //le = positions[i].title;
         marker.setMap(mapLoaction);
+
+        update_Graph2(); 
+        update_Graph3();
     })
 }
 
 /*Graph1*/
-var gaugeOptions = {
+let gaugeOptions = {
   chart: {
       type: 'solidgauge',
       backgroundColor: 'rgba(24, 38, 51, 0.4)'
@@ -158,7 +183,7 @@ var gaugeOptions = {
 };
 
 // The speed gauge
-var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+let chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
   yAxis: {
       min: 0,
       max: 200,
@@ -189,7 +214,7 @@ var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptio
 }));
 
 // The RPM gauge
-var chartRpm = Highcharts.chart('container-rpm', Highcharts.merge(gaugeOptions, {
+let chartRpm = Highcharts.chart('container-rpm', Highcharts.merge(gaugeOptions, {
   yAxis: {
       min: 0,
       max: 5,
@@ -220,7 +245,7 @@ var chartRpm = Highcharts.chart('container-rpm', Highcharts.merge(gaugeOptions, 
 // Bring life to the dials
 setInterval(function () {
   // Speed
-  var point,
+  let point,
       newVal,
       inc;
 
@@ -250,72 +275,245 @@ setInterval(function () {
   }
 }, 2000);
 
+ /*Graph2*/
+function update_Graph2() { 
+    var js_timer2 = 0; 
+    Highcharts.chart('container2', {
+        chart: {
+            type: 'bar',
+            backgroundColor: 'rgba(24, 38, 51, 0.4)'
+        },
+        title: {
+            text: 'Historic World Population by Region'
+        },
+        subtitle: {
+            text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+        },
+        xAxis: {
+            categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Population (millions)',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' millions'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 80,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor:
+                Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Year 1800',
+            data: [arr1[0],arr1[1],arr1[2],arr1[3],arr1[4]]
+        }, {
+            name: 'Year 1900',
+            data: [arr2[0],arr2[1],arr2[2],arr2[3],arr2[4]]
+        }, {
+            name: 'Year 2000',
+            data: [arr3[0],arr3[1],arr3[2],arr3[3],arr3[4]]
+        }, {
+            name: 'Year 2016',
+            data: [arr4[0],arr4[1],arr4[2],arr4[3],arr4[4]]
+        }]
+    });
+    //js_timer2 = setTimeout("update_Graph2()", 5000);// 5초 단위로 갱신 처리
+}
 
-/*Graph2*/
-Highcharts.chart('container2', {
-  chart: {
-      type: 'bar',
-      backgroundColor: 'rgba(24, 38, 51, 0.4)'
-  },
-  title: {
-      text: 'Historic World Population by Region'
-  },
-  subtitle: {
-      text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
-  },
-  xAxis: {
-      categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
-      title: {
-          text: null
-      }
-  },
-  yAxis: {
-      min: 0,
-      title: {
-          text: 'Population (millions)',
-          align: 'high'
-      },
-      labels: {
-          overflow: 'justify'
-      }
-  },
-  tooltip: {
-      valueSuffix: ' millions'
-  },
-  plotOptions: {
-      bar: {
-          dataLabels: {
-              enabled: true
-          }
-      }
-  },
-  legend: {
-      layout: 'vertical',
-      align: 'right',
-      verticalAlign: 'top',
-      x: -40,
-      y: 80,
-      floating: true,
-      borderWidth: 1,
-      backgroundColor:
-          Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
-      shadow: true
-  },
-  credits: {
-      enabled: false
-  },
-  series: [{
-      name: 'Year 1800',
-      data: [107, 31, 635, 203, 2]
-  }, {
-      name: 'Year 1900',
-      data: [133, 156, 947, 408, 6]
-  }, {
-      name: 'Year 2000',
-      data: [814, 841, 3714, 727, 31]
-  }, {
-      name: 'Year 2016',
-      data: [1216, 1001, 4436, 738, 40]
-  }]
-});
+/*Graph3*/
+function update_Graph3() {
+    Highcharts.chart('container3', {
+        chart: {
+            zoomType: 'xy',
+            backgroundColor: 'rgba(24, 38, 51, 0.4)'
+        },
+        title: {
+            text: 'Temperature vs Rainfall'
+        },
+        xAxis: [{
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        }],
+        yAxis: [{ // Primary yAxis
+            labels: {
+                format: '{value} °C',
+                style: {
+                    color: Highcharts.getOptions().colors[3]
+                }
+            },
+            title: {
+                text: 'Temperature',
+                style: {
+                    color: Highcharts.getOptions().colors[3]
+                }
+            }
+        }, { // Secondary yAxis
+            title: {
+                text: 'Rainfall',
+                style: {
+                    color: Highcharts.getOptions().colors[0]
+                }
+            },
+            labels: {
+                format: '{value} mm',
+                style: {
+                    color: Highcharts.getOptions().colors[0]
+                }
+            },
+            opposite: true
+        }],
+    
+        tooltip: {
+            shared: true
+        },
+    
+        series: [{
+            name: 'Rainfall',
+            type: 'column',
+            yAxis: 1,
+            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+            tooltip: {
+                pointFormat: '<span style="font-weight: bold; color: {series.color}">{series.name}</span>: <b>{point.y:.1f} mm</b> '
+            }
+        }, {
+            name: 'Rainfall error',
+            type: 'errorbar',
+            yAxis: 1,
+            data: [[48, 51], [68, 73], [92, 110], [128, 136], [140, 150], [171, 179], [135, 143], [142, 149], [204, 220], [189, 199], [95, 110], [52, 56]],
+            tooltip: {
+                pointFormat: '(error range: {point.low}-{point.high} mm)<br/>'
+            }
+        }, {
+            name: 'Temperature',
+            type: 'spline',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+            tooltip: {
+                pointFormat: '<span style="font-weight: bold; color: {series.color}">{series.name}</span>: <b>{point.y:.1f}°C</b> '
+            }
+        }, {
+            name: 'Temperature error',
+            type: 'errorbar',
+            data: [[6, 8], [5.9, 7.6], [9.4, 10.4], [14.1, 15.9], [18.0, 20.1], [21.0, 24.0], [23.2, 25.3], [26.1, 27.8], [23.2, 23.9], [18.0, 21.1], [12.9, 14.0], [7.6, 10.0]],
+            tooltip: {
+                pointFormat: '(error range: {point.low}-{point.high}°C)<br/>'
+            }
+        }]
+    });
+    
+}
 
+/*Graph4*/
+function update_Graph4() {
+    Highcharts.chart('container4', {
+        chart: {
+            type: 'area',
+            backgroundColor: 'rgba(24, 38, 51, 0.4)'
+        },
+        accessibility: {
+            description: 'Image description: An area chart compares the nuclear stockpiles of the USA and the USSR/Russia between 1945 and 2017. The number of nuclear weapons is plotted on the Y-axis and the years on the X-axis. The chart is interactive, and the year-on-year stockpile levels can be traced for each country. The US has a stockpile of 6 nuclear weapons at the dawn of the nuclear age in 1945. This number has gradually increased to 369 by 1950 when the USSR enters the arms race with 6 weapons. At this point, the US starts to rapidly build its stockpile culminating in 32,040 warheads by 1966 compared to the USSR’s 7,089. From this peak in 1966, the US stockpile gradually decreases as the USSR’s stockpile expands. By 1978 the USSR has closed the nuclear gap at 25,393. The USSR stockpile continues to grow until it reaches a peak of 45,000 in 1986 compared to the US arsenal of 24,401. From 1986, the nuclear stockpiles of both countries start to fall. By 2000, the numbers have fallen to 10,577 and 21,000 for the US and Russia, respectively. The decreases continue until 2017 at which point the US holds 4,018 weapons compared to Russia’s 4,500.'
+        },
+        title: {
+            text: 'US and USSR nuclear stockpiles'
+        },
+        subtitle: {
+            text: 'Sources: <a href="https://thebulletin.org/2006/july/global-nuclear-stockpiles-1945-2006">' +
+                'thebulletin.org</a> &amp; <a href="https://www.armscontrol.org/factsheets/Nuclearweaponswhohaswhat">' +
+                'armscontrol.org</a>'
+        },
+        xAxis: {
+            allowDecimals: false,
+            labels: {
+                formatter: function () {
+                    return this.value; // clean, unformatted number for year
+                }
+            },
+            accessibility: {
+                rangeDescription: 'Range: 1940 to 2017.'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Nuclear weapon states',
+                style: {
+                    color: Highcharts.getOptions().colors[2]
+                }
+            },
+            labels: {
+                formatter: function () {
+                    return this.value / 1000 + 'k';
+                },
+                style: {
+                    color: Highcharts.getOptions().colors[2]
+                }
+            }
+        },
+        tooltip: {
+            pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+        },
+        plotOptions: {
+            area: {
+                pointStart: 1940,
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'USA',
+            data: [
+                null, null, null, null, null, 6, 11, 32, 110, 235,
+                369, 640, 1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468,
+                20434, 24126, 27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342,
+                26662, 26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
+                24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586, 22380,
+                21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950, 10871, 10824,
+                10577, 10527, 10475, 10421, 10358, 10295, 10104, 9914, 9620, 9326,
+                5113, 5113, 4954, 4804, 4761, 4717, 4368, 4018
+            ]
+        }, {
+            name: 'USSR/Russia',
+            data: [null, null, null, null, null, null, null, null, null, null,
+                5, 25, 50, 120, 150, 200, 426, 660, 869, 1060,
+                1605, 2471, 3322, 4238, 5221, 6129, 7089, 8339, 9399, 10538,
+                11643, 13092, 14478, 15915, 17385, 19055, 21205, 23044, 25393, 27935,
+                30062, 32049, 33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000,
+                37000, 35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
+                21000, 20000, 19000, 18000, 18000, 17000, 16000, 15537, 14162, 12787,
+                12600, 11400, 5500, 4512, 4502, 4502, 4500, 4500
+            ]
+        }]
+    });
+}
