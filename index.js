8b36347e476c35797dceb2edb6d1b3740a7ced18
oeanb <oeanb12@gmail.com>
2022-07-21 15:29:03 +0900

    +graph

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ let day = ('0' + today.getDate()).slice(-2);
 let json_data;
 GET_value();
 
+update_Graph1();
 update_Graph2();
-update_Graph3();
-update_Graph4();
 update_time();
 
 let le;
@@ -116,167 +115,13 @@ for(let i = 0; i < 14; i++) {
         //le = positions[i].title;
         marker.setMap(mapLoaction);
 
-        update_Graph2(); 
-        update_Graph3();
+        update_Graph1(); 
+        update_Graph2();
     })
 }
 
 /*Graph1*/
-let gaugeOptions = {
-  chart: {
-      type: 'solidgauge',
-      backgroundColor: 'rgba(24, 38, 51, 0.4)'
-  },
-
-  title: null,
-
-  pane: {
-      center: ['50%', '85%'],
-      size: '140%',
-      startAngle: -90,
-      endAngle: 90,
-      background: {
-          backgroundColor:
-              Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
-          innerRadius: '60%',
-          outerRadius: '100%',
-          shape: 'arc'
-      }
-  },
-
-  exporting: {
-      enabled: false
-  },
-
-  tooltip: {
-      enabled: false
-  },
-
-  // the value axis
-  yAxis: {
-      stops: [
-          [0.1, '#55BF3B'], // green
-          [0.5, '#DDDF0D'], // yellow
-          [0.9, '#DF5353'] // red
-      ],
-      lineWidth: 0,
-      tickWidth: 0,
-      minorTickInterval: null,
-      tickAmount: 2,
-      title: {
-          y: -70
-      },
-      labels: {
-          y: 16
-      }
-  },
-
-  plotOptions: {
-      solidgauge: {
-          dataLabels: {
-              y: 5,
-              borderWidth: 0,
-              useHTML: true
-          }
-      }
-  }
-};
-
-// The speed gauge
-let chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
-  yAxis: {
-      min: 0,
-      max: 200,
-      title: {
-          text: 'Speed'
-      }
-  },
-
-  credits: {
-      enabled: false
-  },
-
-  series: [{
-      name: 'Speed',
-      data: [80],
-      dataLabels: {
-          format:
-              '<div style="text-align:center">' +
-              '<span style="font-size:25px">{y}</span><br/>' +
-              '<span style="font-size:12px;opacity:0.4">km/h</span>' +
-              '</div>'
-      },
-      tooltip: {
-          valueSuffix: ' km/h'
-      }
-  }]
-
-}));
-
-// The RPM gauge
-let chartRpm = Highcharts.chart('container-rpm', Highcharts.merge(gaugeOptions, {
-  yAxis: {
-      min: 0,
-      max: 5,
-      title: {
-          text: 'RPM'
-      }
-  },
-
-  series: [{
-      name: 'RPM',
-      data: [1],
-      dataLabels: {
-          format:
-              '<div style="text-align:center">' +
-              '<span style="font-size:25px">{y:.1f}</span><br/>' +
-              '<span style="font-size:12px;opacity:0.4">' +
-              '* 1000 / min' +
-              '</span>' +
-              '</div>'
-      },
-      tooltip: {
-          valueSuffix: ' revolutions/min'
-      }
-  }]
-
-}));
-
-// Bring life to the dials
-setInterval(function () {
-  // Speed
-  let point,
-      newVal,
-      inc;
-
-  if (chartSpeed) {
-      point = chartSpeed.series[0].points[0];
-      inc = Math.round((Math.random() - 0.5) * 100);
-      newVal = point.y + inc;
-
-      if (newVal < 0 || newVal > 200) {
-          newVal = point.y - inc;
-      }
-
-      point.update(newVal);
-  }
-
-  // RPM
-  if (chartRpm) {
-      point = chartRpm.series[0].points[0];
-      inc = Math.random() - 0.5;
-      newVal = point.y + inc;
-
-      if (newVal < 0 || newVal > 5) {
-          newVal = point.y - inc;
-      }
-
-      point.update(newVal);
-  }
-}, 2000);
-
- /*Graph2*/
-function update_Graph2() { 
+function update_Graph1() { 
     var js_timer2 = 0; 
     Highcharts.chart('container2', {
         chart: {
@@ -350,8 +195,8 @@ function update_Graph2() {
     //js_timer2 = setTimeout("update_Graph2()", 5000);// 5초 단위로 갱신 처리
 }
 
-/*Graph3*/
-function update_Graph3() {
+/*Graph2*/
+function update_Graph2() {
     Highcharts.chart('container3', {
         chart: {
             zoomType: 'xy',
@@ -434,92 +279,116 @@ function update_Graph3() {
     
 }
 
-/*Graph4*/
-function update_Graph4() {
-    Highcharts.chart('container4', {
-        chart: {
-            type: 'area',
-            backgroundColor: 'rgba(24, 38, 51, 0.4)'
-        },
-        accessibility: {
-            description: 'Image description: An area chart compares the nuclear stockpiles of the USA and the USSR/Russia between 1945 and 2017. The number of nuclear weapons is plotted on the Y-axis and the years on the X-axis. The chart is interactive, and the year-on-year stockpile levels can be traced for each country. The US has a stockpile of 6 nuclear weapons at the dawn of the nuclear age in 1945. This number has gradually increased to 369 by 1950 when the USSR enters the arms race with 6 weapons. At this point, the US starts to rapidly build its stockpile culminating in 32,040 warheads by 1966 compared to the USSR’s 7,089. From this peak in 1966, the US stockpile gradually decreases as the USSR’s stockpile expands. By 1978 the USSR has closed the nuclear gap at 25,393. The USSR stockpile continues to grow until it reaches a peak of 45,000 in 1986 compared to the US arsenal of 24,401. From 1986, the nuclear stockpiles of both countries start to fall. By 2000, the numbers have fallen to 10,577 and 21,000 for the US and Russia, respectively. The decreases continue until 2017 at which point the US holds 4,018 weapons compared to Russia’s 4,500.'
+/*Graph3*/
+Highcharts.chart('container4', {
+    chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false,
+        backgroundColor: 'rgba(24, 38, 51, 0.4)'
+    },
+
+    title: {
+        text: 'Speedometer'
+    },
+
+    pane: {
+        startAngle: -150,
+        endAngle: 150,
+        background: [{
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#FFF'],
+                    [1, '#333']
+                ]
+            },
+            borderWidth: 0,
+            outerRadius: '109%'
+        }, {
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#333'],
+                    [1, '#FFF']
+                ]
+            },
+            borderWidth: 1,
+            outerRadius: '107%'
+        }, {
+            // default background
+        }, {
+            backgroundColor: '#DDD',
+            borderWidth: 0,
+            outerRadius: '105%',
+            innerRadius: '103%'
+        }]
+    },
+
+    // the value axis
+    yAxis: {
+        min: 0,
+        max: 200,
+
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+
+        tickPixelInterval: 30,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: {
+            step: 2,
+            rotation: 'auto'
         },
         title: {
-            text: 'US and USSR nuclear stockpiles'
+            text: 'km/h'
         },
-        subtitle: {
-            text: 'Sources: <a href="https://thebulletin.org/2006/july/global-nuclear-stockpiles-1945-2006">' +
-                'thebulletin.org</a> &amp; <a href="https://www.armscontrol.org/factsheets/Nuclearweaponswhohaswhat">' +
-                'armscontrol.org</a>'
-        },
-        xAxis: {
-            allowDecimals: false,
-            labels: {
-                formatter: function () {
-                    return this.value; // clean, unformatted number for year
-                }
-            },
-            accessibility: {
-                rangeDescription: 'Range: 1940 to 2017.'
-            }
-        },
-        yAxis: {
-            title: {
-                text: 'Nuclear weapon states',
-                style: {
-                    color: Highcharts.getOptions().colors[2]
-                }
-            },
-            labels: {
-                formatter: function () {
-                    return this.value / 1000 + 'k';
-                },
-                style: {
-                    color: Highcharts.getOptions().colors[2]
-                }
-            }
-        },
-        tooltip: {
-            pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
-        },
-        plotOptions: {
-            area: {
-                pointStart: 1940,
-                marker: {
-                    enabled: false,
-                    symbol: 'circle',
-                    radius: 2,
-                    states: {
-                        hover: {
-                            enabled: true
-                        }
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'USA',
-            data: [
-                null, null, null, null, null, 6, 11, 32, 110, 235,
-                369, 640, 1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468,
-                20434, 24126, 27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342,
-                26662, 26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
-                24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586, 22380,
-                21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950, 10871, 10824,
-                10577, 10527, 10475, 10421, 10358, 10295, 10104, 9914, 9620, 9326,
-                5113, 5113, 4954, 4804, 4761, 4717, 4368, 4018
-            ]
+        plotBands: [{
+            from: 0,
+            to: 120,
+            color: '#55BF3B' // green
         }, {
-            name: 'USSR/Russia',
-            data: [null, null, null, null, null, null, null, null, null, null,
-                5, 25, 50, 120, 150, 200, 426, 660, 869, 1060,
-                1605, 2471, 3322, 4238, 5221, 6129, 7089, 8339, 9399, 10538,
-                11643, 13092, 14478, 15915, 17385, 19055, 21205, 23044, 25393, 27935,
-                30062, 32049, 33952, 35804, 37431, 39197, 45000, 43000, 41000, 39000,
-                37000, 35000, 33000, 31000, 29000, 27000, 25000, 24000, 23000, 22000,
-                21000, 20000, 19000, 18000, 18000, 17000, 16000, 15537, 14162, 12787,
-                12600, 11400, 5500, 4512, 4502, 4502, 4500, 4500
-            ]
+            from: 120,
+            to: 160,
+            color: '#DDDF0D' // yellow
+        }, {
+            from: 160,
+            to: 200,
+            color: '#DF5353' // red
         }]
-    });
-}
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [80],
+        tooltip: {
+            valueSuffix: ' km/h'
+        }
+    }]
+
+},
+// Add some life
+function (chart) {
+    if (!chart.renderer.forExport) {
+        setInterval(function () {
+            var point = chart.series[0].points[0],
+                newVal,
+                inc = Math.round((Math.random() - 0.5) * 20);
+
+            newVal = point.y + inc;
+            if (newVal < 0 || newVal > 200) {
+                newVal = point.y - inc;
+            }
+
+            point.update(newVal);
+
+        }, 3000);
+    }
+});
